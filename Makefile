20c534f7511cd446b96cf508f2c5e65b5d9fbacf
Requaos <reqpro@requaos>
2022-08-21 19:49:07 +0000

    basic in-memory graph generate and render

--- Makefile
+++ Makefile
@@ -2,4 +2,11 @@ build:
 	wasm-pack build --target web
 
 serve:
-	python3 -m http.server
+	npm run serve
+
+setup:
+	@echo "===> Expecting the following dependencies to be installed:"
+	@echo "     - Rust (https://www.rust-lang.org/tools/install)"
+	@echo "     - npm  (https://docs.npmjs.com/downloading-and-installing-node-js-and-npm)"
+	cargo install wasm-pack
+	npm install
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,53 @@
+use std::borrow::Borrow;
+
+use sophia::graph::{*, inmem::FastGraph};
+use sophia::ns::Namespace;
+use sophia::parser::turtle;
+use sophia::serializer::*;
+use sophia::serializer::nt::NtSerializer;
+use sophia::triple::stream::TripleSource;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+// Called by our JS entry point to run the example
+#[wasm_bindgen(start)]
+pub fn run() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_text_content(Some(graph_it().as_str()));
+
+    body.append_child(&val)?;
+
+    Ok(())
 }
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn graph_it() -> String {
+    let example = r#"
+        @prefix : <http://example.org/>.
+        @prefix foaf: <http://xmlns.com/foaf/0.1/>.
+
+        :alice foaf:name "Alice";
+               foaf:mbox <mailto:alice@work.example> .
+
+        :bob foaf:name "Bob".
+    "#;
+    let mut graph: FastGraph = turtle::parse_str(example).collect_triples().unwrap();
+
+    let ex = Namespace::new("http://example.org/").unwrap();
+    let foaf = Namespace::new("http://xmlns.com/foaf/0.1/").unwrap();
+    graph.insert(
+        &ex.get("bob").unwrap(),
+        &foaf.get("knows").unwrap(),
+        &ex.get("alice").unwrap(),
+    ).unwrap();
+
+    let mut nt_stringifier = NtSerializer::new_stringifier();
+    let example2 = nt_stringifier.serialize_graph(&mut graph).unwrap().as_str();
+
+    return format!("The resulting graph\n{}", example2)
 }
